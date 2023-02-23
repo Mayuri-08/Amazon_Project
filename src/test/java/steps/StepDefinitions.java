53199eb7f513de38d4cefd87c10a6cfcb7f81fba
Mayuri Joshi <mayurijoshiv2000@gmail.com>
2023-02-23 16:26:41 +0530

    New Files added

--- src/test/java/steps/StepDefinitions.java
+++ src/test/java/steps/StepDefinitions.java
@@ -1,32 +1,47 @@
 package steps;
 
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import pages.HomePage;
 import utils.BrowserManager;
 import utils.QaProps;
+import utils.TestDataReader;
+
+import java.util.HashMap;
 
 
 public class StepDefinitions {
     private WebDriver driver;
     HomePage homePage;
     String url;
-    public StepDefinitions(BrowserManager browserManager){
+    HashMap<String, String> data;
+    Scenario scenario;
+    public StepDefinitions(BrowserManager browserManager) {
         this.driver = browserManager.getDriver();
-
-
     }
+        @Before(order = 1)
+        public void before(Scenario scenario){
+            this.scenario = scenario;
+
+        }
+
+
     @Given("the user navigates to home page")
     public void the_user_navigates_to_home_page() {
         homePage = new HomePage(driver);
         url = QaProps.getValue("url");
         driver.get(url);
+       // data = TestDataReader.getData(scenario.getName());
         }
 
     @When("the user enters with the product")
@@ -38,24 +53,33 @@ public class StepDefinitions {
 
     @Then("the user is able to view for a product")
     public void the_user_is_able_to_view_for_a_product() {
+        data = TestDataReader.getData(scenario.getName());
         String text = homePage.getSearchResult().getText();
-        Assert.assertEquals(text,"\"mobiles\"");
+        Assert.assertEquals(text, data.get("Status"));
     }
 
     @And("the user enters min Price")
     public void the_User_Enters_Min_Price() {
-        homePage.getSearchMinPrice().sendKeys("20000");
+        data = TestDataReader.getData(scenario.getName());
+        WebDriverWait wait = new WebDriverWait(driver, 3);
+        wait.until(ExpectedConditions.visibilityOf(homePage.getSearchMinPrice()));
+        homePage.getSearchMinPrice().sendKeys(data.get("Input1"));
     }
 
     @And("the user enters max Price")
     public void the_User_Enters_Max_Price() {
-        homePage.getSearchMaxPrice().sendKeys("25000");
-    }
-
-    @Then("the user is able to view a product according to the range")
-    public void the_User_Is_Able_To_View_A_Product_According_To_The_Range() {
+        WebDriverWait wait = new WebDriverWait(driver, 3);
+        wait.until(ExpectedConditions.visibilityOf(homePage.getSearchMaxPrice()));
+        homePage.getSearchMaxPrice().sendKeys(data.get("Input2"));
         homePage.getGoButton().click();
     }
+    @Then("the user is able to view a product in  selected price range")
+    public void theUserIsAbleToViewAProductInSelectedPriceRange() {
+        data = TestDataReader.getData(scenario.getName());
+        String text = homePage.getViewResult().getText();
+        Assert.assertEquals(text,data.get("Status"));
+    }
+
 
     @And("the user selects from the drop down feature")
     public void the_User_Selects_From_The_Drop_Down_Feature() {
@@ -66,8 +90,9 @@ public class StepDefinitions {
 
     @Then("the user is able to view a product as per selected feature")
     public void the_User_Is_Able_To_View_A_Product_As_Per_Selected_Feature() {
+        data = TestDataReader.getData(scenario.getName());
         String text = homePage.getViewResult().getText();
-        Assert.assertEquals(text,"RESULTS");
+        Assert.assertEquals(text,data.get("Status"));
 
     }
 
@@ -85,6 +110,12 @@ public class StepDefinitions {
     public void the_User_Selects_Cellular_Technology() {
         homePage.getSelectTech().click();
     }
+    @Then("the user is able to view products from the applied categories")
+    public void theUserIsAbleToViewProductsFromTheAppliedCategories() {
+        data = TestDataReader.getData(scenario.getName());
+        String text = homePage.getSearchResult().getText();
+        Assert.assertEquals(text, data.get("Status"));
+    }
 
 
     @When("user searches for {string}")
@@ -98,14 +129,32 @@ public class StepDefinitions {
         homePage.getSearchBox().sendKeys(Keys.ENTER);
 
     }
-    @Then("the item should be applied")
-    public void theItemShouldBeApplied() {
-       // String attribute = homePage.getSearchItem().getText();
-      //  Assert.assertEquals(text,"\"t-shirt adidas\"");
-      //  Assert.assertEquals(text,"\"t-shirt adidas\"");
 
 
+    @Then("it should display all the related searches {string}")
+    public void itShouldDisplayAllTheRelatedSearches(String arg0) {
+        String text = homePage.getSearchItem().getText();
+        Assert.assertTrue(text.contains(arg0));
     }
 
 
+    @When("the user navigates to all section")
+    public void theUserNavigatesToAllSection() {
+        homePage.getSearchAll().click();
+
+    }
+
+    @Then("the user clicks on the books section")
+    public void theUserClicksOnTheBooksSection() throws InterruptedException {
+        Thread.sleep(1000);
+        homePage.getSearchSellers().click();
+        homePage.getSearchBooks().click();
+
+    }
+
+    @And("the user should be able to view the bestseller books")
+    public void theUserShouldBeAbleToViewTheBestsellerBooks() {
+       String text =  homePage.getSearchBestseller().getText();
+        Assert.assertEquals(text,data.get("Status"));
+    }
 }
--- src/test/resources/features/tc1.feature
+++ src/test/resources/features/tc1.feature
@@ -1,46 +1,54 @@
 
 Feature: Amazon Customization
+  Background: User navigates to homepage
+    Given the user navigates to home page
+
 
   Scenario: Verify that the user is able to search for the product
-  Given the user navigates to home page
-  When the user enters with the product
-  Then the user is able to view for a product
+    When the user enters with the product
+    Then the user is able to view for a product
 
 
   Scenario: Verify that user is able to search a product according to her price range
-    Given the user navigates to home page
     When the user enters with the product
     And the user enters min Price
     And the user enters max Price
-    Then the user is able to view a product according to the range
+    Then the user is able to view a product in  selected price range
 
 
     Scenario: Verify that the user is able Select from the dropdown feature after searching for a product
-      Given the user navigates to home page
       When the user enters with the product
       And the user selects from the drop down feature
       Then the user is able to view a product as per selected feature
 
 
       Scenario: Verify that the user is able to select different features from different category for a searched product
-        Given the user navigates to home page
         When the user enters with the product
         And the user selects a OS feature
         And the user selects display type
         And the user selects cellular technology
-        Then the user is able to view for a product
+        Then the user is able to view products from the applied categories
+
 
   Scenario Outline: Customize product on Amazon
-    Given the user navigates to home page
     When user searches for "<product>"
     And user selects the  "<brand>"
-    Then the item should be applied
+    Then it should display all the related searches "<result>"
 
 
     Examples:
-      | product     | brand  |
-      | T-shirt     | Adidas |
-      | Earphones   | Boat   |
+      | product     | brand  | result |
+      | T-shirt     | Adidas | t-shirt adidas |
+      | Earphones   | Boat   | earphones boat |
+
+
+  @smoke
+    Scenario: Verify that the user is able to see the new releases for books section.
+      When the user navigates to all section
+      Then the user clicks on the books section
+      And the user should be able to view the bestseller books
+
+
 
 
 
